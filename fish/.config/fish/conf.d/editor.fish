05ff370cd78694f21eddb35ee9f9f03edef0721f
Ruan Petterson <ruan@petterson.eng.br>
2025-10-18 19:42:49 -0300

    fix(editor): use vim in WSL environment

--- fish/.config/fish/conf.d/editor.fish
+++ fish/.config/fish/conf.d/editor.fish
@@ -1,6 +1,16 @@
 # Preferred editor for local and remote sessions
+set -g -x EDITOR vim
+
 if test -n "$SSH_CONNECTION"
-    set -g -x EDITOR vim
-else
+    return 0
+end
+
+if test -n "$WSL_DISTRO_NAME"
+    return 0
+end
+
+if type -q hx
     set -g -x EDITOR hx
+else if type -q helix
+    set -g -x EDITOR helix
 end
